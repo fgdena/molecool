ebaa09490dcba2e8610aa552611db35b6a590dfe
Dena <loipde@hotmail.com>
2022-05-13 18:37:49 -0300

    added pytest functionality and test modules

---
+++ molecool/tests/test_angle.py
@@ -0,0 +1,20 @@
+"""
+Unit and regression test for the angle module.
+"""
+
+# Import package, test suite, and other packages as needed
+import molecool
+import pytest
+import numpy as np
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle", [
+    (np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 45),
+    (np.array([0, 0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60  ),
+    (np.array([np.sqrt(3)/2, (1/2), 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 30),
+])
+def test_calculate_angle_many(p1, p2, p3, expected_angle):
+    """Test that calculate_angle function calculates what we expect."""
+
+    calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+
+    assert expected_angle == pytest.approx(calculated_angle), F'{calculated_angle} {expected_angle}'
